834696b8d9d1d0e553584a84a3ecacef882b814e
erikphinguyen <erikphinguyen@gmail.com>
2022-08-02 22:30:20 -0700

    changed backend/routes/index.js to correct code

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,41 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const apiRouter = require("./api");
 
-const apiRouter = require('./api');
+router.use("/api", apiRouter);
 
-router.use('/api', apiRouter);
+// router.get("/hello/world", function (req, res) {
+//   res.cookie("XSRF-TOKEN", req.csrfToken());
+//   res.send("Hello World!");
+// });
 
-router.get('/hello/world', function (req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
+// Static routes
+// Serve React build files in production
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  // Serve the frontend's index.html file at the root route
+  router.get("/", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve("../frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    );
+  });
+}
+
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== "production") {
+  router.get("/api/csrf/restore", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    res.status(201).json({});
+  });
+}
 
 module.exports = router;
-
-/*
-USING csurfMiddleWare from middleware (utils) folder:
-const csurfMiddleWare = require('../utils/auth')
-router.get('/hello/world', csurfMiddleWare, function (req, res) {
-    // console.log(req)
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
-*/
-
-/*
-this file could also be written as
-
-const router = require('express').Router();
-const sessionRouter = require('./session.js');
-const usersRouter = require('./users.js');
-
-router.use('/session', sessionRouter);
-
-router.use('/users', usersRouter);
-
-router.post('/test', (req, res) => {
-  res.json({ requestBody: req.body });
-});
-
-module.exports = router;
-*/
