8c565ae57dda1977888b685d18f77ea92f6cc453
erikphinguyen <erikphinguyen@gmail.com>
2022-08-08 08:44:46 -0700

    backend reviews done possibly?

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -3,44 +3,41 @@ const router = express.Router();
 const asyncHandler = require('express-async-handler');
 
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
-const { User, Image } = require('../../db/models');
 
-const { check, validationResult } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
 const { db } = require('../../config');
-const { Review } = require('../../db/models')
+const { User, Brand, Review } = require('../../db/models')
 
 // GET ALL REVIEWS OF CERTAIN BRAND
 router.get('/:id(\\d+)', requireAuth, restoreUser, asyncHandler(async (req, res) => {
     const { id } = req.params;
     const brand = await Brand.findByPk(id);
-    let reviews = await Comment.findAll({
+    let reviews = await Review.findAll({
         where: { id: id }
     });
 
     return res.json(reviews);
 }));
 
-// POST COMMENT
+// POST REVIEW
 router.post('/:id(\\d+)', requireAuth, restoreUser, asyncHandler(async (req, res) => {
     const { id } = req.params;
-    const { content, imageId } = req.body;
-    const newComment = await Comment.create({
-        content, imageId
+    const { brandImg, name, address, city, country } = req.body;
+    const newReview = await Review.create({
+        brandImg, name, address, city, country
     });
-    // const newComment = new Comment({ content, id})
-    // await newComment.save()
-    const payload = await Comment.findByPk(newComment.id)
+    // const newReview = new Review({ id, brandImg, name, address, city, country})
+    // await newReview.save()
+    const payload = await Review.findByPk(newReview.id)
     return res.json(payload);
 }))
 
-//  // DELETE COMMENT
+// DELETE REVIEW
 router.delete('/:id(\\d+)', requireAuth, restoreUser, asyncHandler(async (req, res) => {
     const { id } = req.params;
-    const comment = await Comment.findByPk(id)
-    await comment.destroy();
+    const review = await Review.findByPk(id)
+    await review.destroy();
     res.json({
-        message: "comment deleted"
+        message: "review deleted"
     })
 }))
 
