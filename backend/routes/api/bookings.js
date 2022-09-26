bf55cc8ebf9b9da03225ca5adc4009b23b1002d1
erikphinguyen <erikphinguyen@gmail.com>
2022-09-26 16:48:41 -0700

    get bookings done

---
+++ backend/routes/api/bookings.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const asyncHandler = require('express-async-handler');
+const { requireAuth } = require('../../utils/auth.js')
+// forgot if we need line 6?
+const db = require('../../db/models')
+
+// GET BOOKINGS
+router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
+    console.log('INSIDE BOOKINGS ROUTE');
+    const { id } = req.params;
+    // first option with .findAll
+    const bookings = await Bookings.findAll(
+        {
+            where: { id }
+        }
+    )
+    // second option with .findByPk
+    // const bookings = await Bookings.findByPk(Number(id))
+
+    return res.json(bookings)
+}))
