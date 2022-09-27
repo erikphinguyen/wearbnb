23eca0c01c891ef7a7c448d932b04dbb995de9d8
erikphinguyen <erikphinguyen@gmail.com>
2022-09-26 17:22:27 -0700

    bookings models done

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -1,15 +1,22 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Booking = sequelize.define('Booking', {
-    brandId: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER,
+    brandId: {
+      type: DataTypes.INTEGER,
+      references: { model: "Brands" }
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: { model: "Users" }
+    },
     startDate: DataTypes.DATE,
     endDate: DataTypes.DATE,
     price: DataTypes.FLOAT,
     totalPrice: DataTypes.FLOAT
   }, {});
-  Booking.associate = function(models) {
-    // associations can be defined here
+  Booking.associate = function (models) {
+    Booking.belongsTo(models.Brand, { foreignKey: "brandId" })
+    Booking.belognsTo(models.User, { foreignKey: "userId" })
   };
   return Booking;
 };
