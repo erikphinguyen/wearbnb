5f520edb4d30f28fedaed2c65dcb03e9e058b354
erikphinguyen <erikphinguyen@gmail.com>
2022-10-05 23:42:08 -0700

    boiler plate added

--- frontend/src/components/Bookings/UserBookings.js
+++ frontend/src/components/Bookings/UserBookings.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 function UserBookings() {
+    const dispatch = useDispatch();
+    const { id } = useParams();
+
+
+
+
     return (
         <div>UserBookings</div>
     )
