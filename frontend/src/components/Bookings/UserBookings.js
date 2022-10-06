727c1f89bf6336865dc25a9933b8c167556c57c2
erikphinguyen <erikphinguyen@gmail.com>
2022-10-05 23:41:36 -0700

    started UserBookings.js

---
+++ frontend/src/components/Bookings/UserBookings.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+function UserBookings() {
+    return (
+        <div>UserBookings</div>
+    )
+}
+
+export default UserBookings
