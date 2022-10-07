76f4f5987183a4c8436dd1b2d09890473bac0a80
erikphinguyen <erikphinguyen@gmail.com>
2022-10-06 18:22:52 -0700

    maybe UserBookings.js is ready?

--- frontend/src/components/Bookings/UserBookings.js
+++ frontend/src/components/Bookings/UserBookings.js
@@ -7,11 +7,15 @@ function UserBookings() {
     const dispatch = useDispatch();
     const { id } = useParams();
     const bookings = useSelector(state => state.bookings[id]);
+    const user = useSelector(state.session.user?.id)
+    const username = useSelector(state => state.session.user?.username)
+    const brandName = useSelector(state => state.brands[id]?.name);
 
     useEffect(() => {
         dispatch(thunkGetUserBookings(id))
     }, [id, dispatch])
 
+    // this returns an array of bookings from specific user
     const bookingsFromUser = Object.values(bookings)
 
     let stayDuration = (startDate, endDate) => {
@@ -19,7 +23,24 @@ function UserBookings() {
     }
 
     return (
-        <div>UserBookings</div>
+        <div className='user-bookings-container'>
+            <h1>
+                User {user}'s Bookings
+            </h1>
+            {bookingsFromUser && bookingsFromUser.map(booking => (
+                <div className='user-bookings'>
+                    <p>
+                        {brandName}
+                    </p>
+                    <p>
+                        {`${booking.price} / ${stayDuration(booking.startDate, booking.endDate)}`}
+                    </p>
+                    <p>
+                        {`Total: ${booking.totalPrice}`}
+                    </p>
+                </div>
+            ))}
+        </div>
     )
 }
 
