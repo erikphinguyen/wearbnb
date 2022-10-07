8d72759251ec83fc12fc4813699bdeb472a433a9
erikphinguyen <erikphinguyen@gmail.com>
2022-10-06 17:57:42 -0700

    added onto UserBookings.js

--- frontend/src/components/Bookings/UserBookings.js
+++ frontend/src/components/Bookings/UserBookings.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, NavLink } from "react-router-dom";
+import { thunkGetUserBookings } from '../../store/bookings';
 
 function UserBookings() {
     const dispatch = useDispatch();
     const { id } = useParams();
     const bookings = useSelector(state => state.bookings[id]);
 
+    useEffect(() => {
+        dispatch(thunkGetUserBookings(id))
+    }, [id, dispatch])
 
+    const bookingsFromUser = Object.values(bookings)
 
     return (
         <div>UserBookings</div>
