6d79b488ed6d29b4fd7657a3eabefdd2b16f2057
erikphinguyen <erikphinguyen@gmail.com>
2022-10-22 21:55:54 -0700

    need finishing touches for search bar, but done

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -4,9 +4,11 @@ import './SearchBar.css';
 function SearchBar({ placeholder, data }) {
 
     const [filteredData, setFilteredData] = useState([]);
+    const [brandEntered, setBrandEntered] = useState('');
 
     const handleFilter = (e) => {
         const searchBrand = e.target.value;
+        setWordEntered(searchBrand)
         const newFilter = data.filter((value) => {
             return value.name.toLowerCase().includes(searchBrand.toLowerCase())
         })
@@ -18,12 +20,22 @@ function SearchBar({ placeholder, data }) {
         }
     }
 
+    const clearInput = () => {
+        setFilteredData([]);
+        setWordEntered('')
+    }
+
     return (
         <div className='search'>
             <div className='searchInputs'>
-                <input type='text' placeholder={placeholder} onChange={handleFilter} />
+                <input
+                    type='text'
+                    placeholder={placeholder}
+                    value={brandEntered}
+                    onChange={handleFilter} />
                 <div className='searchIcon'>
-                    Search
+                    {/*search bar react tutorial ~28 min */}
+                    {filteredData.length === 0 ? <p>Search Icon here</p> : <p>Close Icon here</p>}
                 </div>
             </div>
             {(filteredData.length !== 0) && (
