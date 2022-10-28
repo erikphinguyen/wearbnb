4805ae888932cd8b587ce884b2c157fcf36c810a
erikphinguyen <erikphinguyen@gmail.com>
2022-10-27 18:56:45 -0700

    added fs

--- backend/routes/api/uploads.js
+++ backend/routes/api/uploads.js
@@ -2,4 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: 'uploads/' });
+
+const fs = require('fs');
+const util = require('util');
+const unlinkFile = util.promisify(fs.unlink);
