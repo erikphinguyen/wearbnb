e1eb8a57c1950766c71565babd108d2763aba3c9
erikphinguyen <erikphinguyen@gmail.com>
2022-10-27 18:58:24 -0700

    added video and photo

--- backend/routes/api/uploads.js
+++ backend/routes/api/uploads.js
@@ -7,3 +7,8 @@ const upload = multer({ dest: 'uploads/' });
 const fs = require('fs');
 const util = require('util');
 const unlinkFile = util.promisify(fs.unlink);
+
+const {uploadFile, getFile, deleteFile} = require('../../s3')
+
+const Video = require('../../db/models/Video')
+const Photo = require('../../db/models/Photo')
