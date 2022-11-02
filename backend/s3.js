325acfc34462f9ab46c1f4eed2b21dc87bc0bc21
erikphinguyen <erikphinguyen@gmail.com>
2022-11-01 21:23:23 -0700

    added uploadFile function

--- backend/s3.js
+++ backend/s3.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const S3 = require('aws-sdk/clients/s3');
 const fs = require('fs');
 
@@ -9,3 +10,24 @@ const bucketName = process.env.AWS_BUCKET_NAME;
 const region = process.env.AWS_BUCKET_REGION;
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_KEY;
+
+const s3 = new S3({
+    region,
+    accessKeyId,
+    secretAccessKey
+})
+
+// uploads a file to s3
+const uploadFile = (file) => {
+    const fileStream = fs.createReadStream(file.path);
+
+    const uploadParams = {
+        Bucket: bucketName,
+        Body: fileStream,
+        Key: file.filename
+    }
+
+    return s3.upload(uploadParams).promise();
+}
+
+exports.uploadFile = uploadFile;
