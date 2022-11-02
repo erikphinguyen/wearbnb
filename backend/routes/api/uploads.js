f2059c0c2ad8c1dbf1399231e0be356798b15821
erikphinguyen <erikphinguyen@gmail.com>
2022-11-01 19:04:33 -0700

    upoads.js route done

--- backend/routes/api/uploads.js
+++ backend/routes/api/uploads.js
@@ -10,5 +10,23 @@ const unlinkFile = util.promisify(fs.unlink);
 
 const {uploadFile, getFile, deleteFile} = require('../../s3')
 
-const Video = require('../../db/models/Video')
 const Photo = require('../../db/models/Photo')
+const Video = require('../../db/models/Video')
+
+router.post('/', upload.single('video'), async (req, res) => {
+    const file = req.file;
+    const photoId = req.body.photoId;
+    const result = await uploadFile(file);
+
+    if (result) {
+        let photo = new Photo({
+            photo: photoId,
+            filepath: result.Key
+        })
+        await Photo.findByIdAndUpdate(photoId, {visited: true});
+        await photo.save();
+    }
+
+    await unlinkFile(file.path);
+    res.send({photoPath: `/uploads/${result.Key}`});
+})
