e543c3c857605657c5d887e1ec3d656973306f8e
erikphinguyen <erikphinguyen@gmail.com>
2022-11-08 18:41:06 -0800

    updated uploads.js store

--- frontend/src/store/uploads.js
+++ frontend/src/store/uploads.js
@@ -1,172 +1,107 @@
 import { csrfFetch } from './csrf';
 
 // TYPES
-const GET_PHOTOS = 'uploads/get_photos';
-const GET_VIDEOS = 'uploads/get_videos';
-const POST_PHOTOS = 'uploads/post_photos';
-const POST_VIDEOS = 'uploads/post_videos';
-const DELETE_PHOTOS = 'uploads/delete_photos';
-const DELETE_VIDEOS = 'uploads/delete_videos';
+const GET_IMAGES = 'images/get_images';
+const GET_ONE_IMAGE = 'images/get_one_image';
+const POST_IMAGES = 'images/post_images';
+const DELETE_IMAGES = 'images/delete_images';
 
-// ACTION CREATORS
-const getPhotos = (photos) => {
+// ACTION
+const getImages = (images) => {
     return {
-        type: GET_PHOTOS,
-        photos
+        type: GET_IMAGES,
+        images
     }
 }
 
-const getVideos = (videos) => {
+const getOneImage = (image) => {
     return {
-        type: GET_VIDEOS,
-        videos
+        type: GET_ONE_IMAGE,
+        image
     }
 }
 
-const postPhotos = (photos) => {
+const postImages = (image) => {
     return {
-        type: POST_PHOTOS,
-        photos
+        type: POST_IMAGES,
+        image
     }
 }
 
-const postVideos = (videos) => {
+const deleteImages = (id) => {
     return {
-        type: POST_VIDEOS,
-        videos
-    }
-}
-
-const deletePhotos = (id) => {
-    return {
-        type: DELETE_PHOTOS,
-        id
-    }
-}
-
-const deleteVideos = (id) => {
-    return {
-        type: DELETE_VIDEOS,
+        type: DELETE_IMAGES,
         id
     }
 }
 
 // THUNKS
-// GET ALL PHOTOS
-export const thunkGetPhotos = () => async (dispatch) => {
-    const response = await csrfFetch(`/api/uploads/photos`)
+export const thunkGetImages = () => async (dispatch) => {
+    const response = await csrfFetch(`api/uploads`);
+
     if (response.ok) {
-        const photos = await response.json();
-        dispatch(getPhotos(photos));
-        return photos
+        const images = await response.json();
+        dispatch(getImages(images));
+        return images;
     }
 }
 
-// GET ALL VIDEOS
-export const thunkGetVideos = () => async (dispatch) => {
-    const response = await csrfFetch(`/api/uploads/videos`)
+export const thunkGetOneImage = (id) => async (dispatch) => {
+    const response = await csrfFetch(`/api/uploads/${id}`)
     if (response.ok) {
-        const videos = await response.json();
-        dispatch(getVideos(videos));
-        return videos
+        const image = await response.json();
+        dispatch(getOneImage(image));
+        return image
     }
 }
 
-export const thunkPostPhotos = (data) => async dispatch => {
-    const response = await csrfFetch(`/api/uploads/photos`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    });
+export const thunkPostImage = (data) => async (dispatch) => {
+    const { images } = data;
 
-    if (response.ok) {
-        const photo = await response.json();
-        dispatch(postPhotos(photo));
-        return photo;
-    }
-    else {
-        const data = await response.json()
-        return data
-    }
-};
+    Array.from(images).forEach(async image => {
+        const formData = new FormData();
+        formData.append('image', image);
 
-export const thunkPostVideos = (data) => async dispatch => {
-    const response = await csrfFetch(`/api/uploads/videos`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    });
+        const response = await csrfFetch(`/api/uploads`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'multipartform-data' },
+            body: formData
+        })
 
-    if (response.ok) {
-        const video = await response.json();
-        dispatch(postVideos(video));
-        return video;
-    }
-    else {
-        const data = await response.json()
-        return data
-    }
-};
-
-export const thunkDeletePhotos = (id) => async dispatch => {
-    const response = await csrfFetch(`/api/uploads/photos/${id}`, {
-        method: 'DELETE',
-    });
-
-    if (response.ok) {
-        const { id }= await response.json();
-        dispatch(deletePhotos(id));
-        return id;
-    }
-};
-export const thunkDeleteVideos = (id) => async dispatch => {
-    const response = await csrfFetch(`/api/uploads/videos/${id}`, {
-        method: 'DELETE',
-    });
-
-    if (response.ok) {
-        const { id }= await response.json();
-        dispatch(deleteVideos(id));
-        return id;
-    }
-};
+        if (response.ok) {
+            let s3Image = await response.json();
+            dispatch(postImages(s3Image))
+            return s3Image
+        }
+        else {
+            const data = await response.json();
+            return data
+        }
+    })
+}
 
 // REDUCER
-// const initialState = { entries: {}, isLoading: true };
-
-const uploadsReducer = (state = {}, action) => {
+const imagesReducer = (state = {}, action) => {
     switch (action.type) {
-        case GET_PHOTOS:
-            const newPhotos = {};
-            action.uploads.forEach(photo => {
-                newPhotos[photo.id] = photo;
+        case GET_IMAGES:
+            const newImage = {};
+            action.images.forEach(image => {
+                newImage[image.id] = image
             })
             return {
-                ...newPhotos
+                ...newImage
             }
-        case GET_VIDEOS:
-            const newVideos = {};
-            action.videos.forEach(video => {
-                newVideos[video.id] = video;
-            })
-            return {
-                ...newVideos
-            }
-            case POST_PHOTOS:
-                const postState = { ...state };
-                postState[action.uploads.id] = action.uploads
-                return postState;
-        case DELETE_PHOTOS:
-            const deleteState = { ...state };
-            delete deleteState[action.id]
-            return deleteState;
+        case GET_ONE_IMAGE:
+            const newState = { ...state };
+            newState[action.image.id] = action.image
+            return newState
+        case POST_BRANDS:
+            const postState = { ...state };
+            postState[action.images.id] = action.images
+            return postState;
         default:
             return state;
     }
-};
+}
 
-export default uploadsReducer;
+export default imagesReducer;
