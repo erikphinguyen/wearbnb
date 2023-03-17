f737cc7f5096644020b4189fb91dd3d3c26d5e82
erikphinguyen <erikphinguyen@gmail.com>
2023-03-16 18:57:10 -0700

    aws s3 finally done

--- backend/db/migrations/20220624164842-create-user.js
+++ backend/db/migrations/20220624164842-create-user.js
@@ -18,6 +18,10 @@ module.exports = {
         unique: true,
         allowNull: false
       },
+      profileImageUrl: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
       hashedPassword: {
         type: Sequelize.STRING,
         allowNull: false
