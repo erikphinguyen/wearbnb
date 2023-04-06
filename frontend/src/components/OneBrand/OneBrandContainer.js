fac45b4e717245ff192b50e9c92f347f9febcb0f
Erik Nguyen <erikphinguyen@gmail.com>
2023-04-05 22:19:08 -0700

    fixed google reference error

--- frontend/src/components/OneBrand/OneBrandContainer.js
+++ frontend/src/components/OneBrand/OneBrandContainer.js
@@ -18,12 +18,9 @@ function OneBrandContainer() {
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
     })
 
-    // if (!isLoaded) {
-    //     return <div>Loading...</div>
-    // }
-
     // get brand
     const [singleBrand, setSingleBrand] = useState({})
+    const center = { lat: 48.8584, lng: 2.2945 }
 
     useEffect(() => {
         dispatch(thunkGetOneBrand(id))
@@ -33,9 +30,14 @@ function OneBrandContainer() {
             .catch(err => console.log(err))
     }, [dispatch, id])
 
+    if (!isLoaded) {
+        return <div>Loading...</div>
+    }
+
+
+
     console.log('WHAT IS SINGLE BRAND', singleBrand)
 
-    const center = { lat: 48.8584, lng: 2.2945 }
 
     return (
         <div className='one-brand-container'>
@@ -46,7 +48,7 @@ function OneBrandContainer() {
             </div>
             <div className='google-map'>
                 {/* <GoogleMaps /> */}
-                <GoogleMap center={center} zoom={15} mapContainerStyle={{ width: '100%', height: '100%' }}>
+                <GoogleMap center={center} zoom={15} mapContainerStyle={{ width: '100%', height: '50%' }}>
 
                 </GoogleMap>
             </div>
